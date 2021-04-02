4e32c921a77e128009542fc4e671a2d4ac5dc182
Takaharayuuki <yuuki.barber@gmail.com>
2021-04-02 18:24:20 +0900

    途中

--- googlemap-sample/vue-google-map/src/components/GoogleMap.vue
+++ googlemap-sample/vue-google-map/src/components/GoogleMap.vue
@@ -1,41 +1,62 @@
 <template>
   <div>
     <h1>Google Map</h1>
-    <div ref="map" style="height: 500px; width:800px;"></div>
+    <input type="text" v-model="address" >
+    <button type="button" @click="mapSearch">検索</button>
+    <div id="map"></div>
   </div>
 </template>
 
 <script>
 export default {
-  props: {
-    myLatLng: {
-      type: Object,
-      required:true,
-     },
-     zoom: {
-       type:Number,
-       required: true
-     }
-  },
   data(){
     return {
-      map: '',
+      map: {},
+      marker: null,
+      geocoder: {},
+      address:'',
+      myLatLng:{},
+      lat:'',
+      lng:''
     }
   },
   mounted(){
-    console.log(this.key);
-    let script = document.createElement('script');
-    script.src = process.env.VUE_APP_API_MAP;
-    script.async = true;
-    document.head.appendChild(script);
+    if(!window.mapLoadStarted){
+      window.mapLoadStarted = true;
+      let script = document.createElement('script');
+      script.src = process.env.VUE_APP_API_MAP;
+      script.async = true;
+      document.head.appendChild(script);
+    }
 
     window.initMap = () => {
-      this.map = new window.google.maps.Map(this.$refs.map,{
-        center: this.myLatLng,
-        zoom: this.zoom
-      });
-      new window.google.maps.Marker({position:this.myLatLng,map:this.map})
+      window.mapLoaded = true;
+      this.map = new window.google.maps.Map(document.getElementById('map'))
+      this.geocoder = new window.google.maps.Geocoder();
     }
-  }
-}
+  },
+  methods: {
+    mapSearch(){
+      this.geocoder.geocode({
+        'address': this.address
+      }, (results, status) => {
+          if(status === window.google.maps.GeocoderStatus.OK) {
+            this.map.setCenter(results[0].geometry.location);
+            this.lat = results[0].geometry.location.lat();
+            this.lng = results[0].geometry.location.lng();
+            console.log(this.lat);
+            console.log(this.lng);
+            this.myLatLng.lat = this.lat;
+            this.myLatLng.lng = this.lng;
+            console.log(this.myLatLng);
+            this.map = new window.google.maps.Map(this.$refs.map, {
+              center: this.myLatLng,
+              zoom: 8
+            });
+            new window.google.maps.Marker({position:this.myLatLng,map:this.map})
+        }
+      });
+    }
+  },
+};
 </script>
