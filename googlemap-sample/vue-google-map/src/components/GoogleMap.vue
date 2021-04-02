e995d78861cb76d9826fe2b48744979d833eaed9
Takaharayuuki <yuuki.barber@gmail.com>
2021-04-02 14:05:56 +0900

    googlemap api sample

---
+++ googlemap-sample/vue-google-map/src/components/GoogleMap.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Google Map</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted(){
+    console.log(window.google)
+    setTimeout(() => {
+      if(!window.google){
+        console.log('ロードに時間がかかっています。');
+      } else {
+        console.log(window.google);
+      }
+    },3000)
+  }
+}
+</script>
