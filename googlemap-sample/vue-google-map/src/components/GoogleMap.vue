3ebd0b23f856064604376786c351f7e9600adcf6
Takaharayuuki <takahara.yuuki22@gmail.com>
2021-04-03 16:36:50 +0900

    ひとまず完成　精度が悪い

--- googlemap-sample/vue-google-map/src/components/GoogleMap.vue
+++ googlemap-sample/vue-google-map/src/components/GoogleMap.vue
@@ -1,29 +1,29 @@
 <template>
   <div>
     <h1>Google Map</h1>
-    <input type="text" v-model="address" >
+    <input type="text" v-model="address" />
     <button type="button" @click="mapSearch">検索</button>
-    <div id="map"></div>
+    <div ref="map" id="map" style="height: 800px; width: 800px"></div>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
       map: {},
       marker: null,
       geocoder: {},
-      address:'',
-      myLatLng:{},
-      lat:'',
-      lng:''
-    }
+      address: "",
+      myLatLng: {},
+      lat: "",
+      lng: "",
+    };
   },
-  mounted(){
-    if(!window.mapLoadStarted){
+  mounted() {
+    if (!window.mapLoadStarted) {
       window.mapLoadStarted = true;
-      let script = document.createElement('script');
+      let script = document.createElement("script");
       script.src = process.env.VUE_APP_API_MAP;
       script.async = true;
       document.head.appendChild(script);
@@ -31,32 +31,35 @@ export default {
 
     window.initMap = () => {
       window.mapLoaded = true;
-      this.map = new window.google.maps.Map(document.getElementById('map'))
+      this.map = new window.google.maps.Map(document.getElementById("map"));
       this.geocoder = new window.google.maps.Geocoder();
-    }
+    };
   },
   methods: {
-    mapSearch(){
-      this.geocoder.geocode({
-        'address': this.address
-      }, (results, status) => {
-          if(status === window.google.maps.GeocoderStatus.OK) {
-            this.map.setCenter(results[0].geometry.location);
-            this.lat = results[0].geometry.location.lat();
-            this.lng = results[0].geometry.location.lng();
+    mapSearch() {
+      this.map = new window.google.maps.Map(document.getElementById("map"));
+      this.geocoder = new window.google.maps.Geocoder();
+      this.geocoder.geocode(
+        {
+          address: this.address,
+        },
+        (results, status) => {
+          if (status === window.google.maps.GeocoderStatus.OK) {
+            this.lat = parseFloat(results[0].geometry.location.lat());
+            this.lng = parseFloat(results[0].geometry.location.lng());
             console.log(this.lat);
             console.log(this.lng);
             this.myLatLng.lat = this.lat;
             this.myLatLng.lng = this.lng;
-            console.log(this.myLatLng);
-            this.map = new window.google.maps.Map(this.$refs.map, {
-              center: this.myLatLng,
-              zoom: 8
-            });
-            new window.google.maps.Marker({position:this.myLatLng,map:this.map})
+            console.log(typeof this.lng);
+          }
         }
+      );
+      this.map = new window.google.maps.Map(this.$refs.map, {
+        center: { lat: +this.lat, lng: +this.lng },
+        zoom: 14,
       });
-    }
+    },
   },
 };
 </script>
